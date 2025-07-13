8a2e9350b6af7277fb1d63c7929462f3b70d3b82
Ansubha Dhar <ansubhadhar2012@gmail.com>
2025-07-13 20:22:35 +0530

    fixed error in backend of form

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -2,48 +2,41 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
-
-
-const SendMail = async (name, email,subject, message) => {
+const SendMail = async (name, email, subject, message) => {
+  try {
     const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
-        auth: {
-            user: process.env.HOST_EMAIL,
-            pass: process.env.HOST_PASSKEY
-        }
-    })
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.HOST_EMAIL,
+        pass: process.env.HOST_PASSKEY,
+      },
+    });
 
     const mail1 = {
-        from :process.env.HOST_EMAIL,
-        to:process.env.HOST_EMAIL,
-        subject: `${subject} - ${name} - ${email}`,
-        text: message
-    }
+      from: process.env.HOST_EMAIL,
+      to: process.env.HOST_EMAIL,
+      subject: `${subject} - ${name} - ${email}`,
+      text: message,
+    };
 
     const mail2 = {
-        from: process.env.HOST_EMAIL,
-        to: email,
-        subject: `Thank You ${name} for contacting Bigyanlabs`,
-        text: `Dear ${name},\n\nThank you for reaching out to BigyanLabs.\nWe have received your query & our team will get back to you shortly with the necessary information or support. If your query is urgent, please feel free to reach out to us directly at .\n\nWarm regards,\nTeam BigyanLabs`
-    }
+      from: process.env.HOST_EMAIL,
+      to: email,
+      subject: `Thank You ${name} for contacting BigyanLabs`,
+      text: `Dear ${name},\n\nThank you for reaching out to BigyanLabs.\nWe have received your query and our team will get back to you shortly.\n\nWarm regards,\nTeam BigyanLabs`,
+    };
 
-    await transporter.sendMail(mail1,(error,info)=>{
-        if(error)
-            console.log("Error in mail 1")
-        else
-        console.log("Message 2 mailed successfully...");
-    })
+    const info1 = await transporter.sendMail(mail1);
+    console.log("Message 1 sent");
 
-    await transporter.sendMail(mail2,(error,info)=>{
-        if(error)
-            console.log("Error in mail 2")
-        else
-        console.log("Message 2 mailed successfully...");
-    })
-
-
-}
+    const info2 = await transporter.sendMail(mail2);
+    console.log("Message 2 sent");
+    
+  } catch (error) {
+    console.error("Error while sending emails:", error);
+  }
+};
 
 export default SendMail;
